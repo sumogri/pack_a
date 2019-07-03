52a295f17c51e119224447c1607fb5031679b244
kazushi sakashita <kazusi@hotmail.co.jp>
2019-07-03 22:17:28 +0900

    [add] オンラインランキング、ツイートのSDKを追加
    使うかはわかんない

--- UnityProject/Pack_A/Assets/Pack_a/Scripts/AGenerater.cs
+++ UnityProject/Pack_A/Assets/Pack_a/Scripts/AGenerater.cs
@@ -1,8 +1,32 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class AGenerater : MonoBehaviour
 {
+    [SerializeField] private GameObject aPrefab;
+    [SerializeField] private bool isGenerate = true;
+    [SerializeField] private float spanSec = 1;
+    private float time = 0;
+    
+    private void Update()
+    {
+        time += Time.deltaTime;
 
+        if (!isGenerate || time < spanSec)
+            return;
+
+        time = 0;
+        Instantiate(aPrefab, transform);
+    }
+
+    [Conditional("UNITY_EDITOR")]
+    private void OnDrawGizmos()
+    {
+        
+    }
 }
