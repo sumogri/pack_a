bb1016dc152dabfefd78a309a6c312734469dd27
kazushi sakashita <kazusi@hotmail.co.jp>
2019-07-07 00:35:58 +0900

    [add] タイトルモックを作成

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 Unity 1 week game jam 「あつめる」投稿作品
 
 ## 使用アセット
-このプロジェクトには以下のアセットを使用しています。  
+このプロジェクトには以下のアセットを含んでいます。  
 
 - ワープロ明朝 , irori
 - はらませにゃんこ , 稲塚 春
 - 無心 , MODI工場／倒神神倒
+
+また、以下のアセットを使用して作成された画像等を含みます。  
+
+- Photoshop Splatter Brushes , konakonakona , CC BY-NC-ND 3.0
---
+++ UnityProject/Pack_A/Assets/Pack_a/Scripts/TitleAGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UniRx;
+
+public class TitleAGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject aPrefab = null;
+    [SerializeField] private bool isGenerate = true;
+    [SerializeField] private float spanSec = 1;
+    [SerializeField] private int maxGenerate = 15;
+    private int nowGenerateCounter = 0;
+    private float time = float.PositiveInfinity;
+
+    private void Start()
+    {
+        time = spanSec;
+    }
+
+    private void Update()
+    {
+        if (!isGenerate) return;
+
+        time += Time.deltaTime;
+
+        if (time < spanSec)
+            return;
+
+        time = 0;
+        var i = Instantiate(aPrefab, transform.parent);
+        i.transform.position = transform.position;
+
+        nowGenerateCounter++;
+        //指定個数生成したら、休止
+        if (nowGenerateCounter >= maxGenerate)
+        {
+            Init();
+        }
+    }
+
+    private void Init()
+    {
+        isGenerate = false;
+        nowGenerateCounter = 0;
+        time = spanSec;
+    }
+
+    [Conditional("UNITY_EDITOR")]
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, Vector3.one);
+    }
+}
