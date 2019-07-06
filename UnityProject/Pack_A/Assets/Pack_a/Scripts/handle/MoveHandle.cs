5f28c07f4cec41b31b0e2cac5577ead74e3ba5f6
kazushi sakashita <kazusi@hotmail.co.jp>
2019-07-06 14:54:01 +0900

    [add] ハンドルに回転機能を追加 / ハンドル操作をパーツ全体で取るように変更 / 白版パーツ(色変更に対応できる)を追加

---
+++ UnityProject/Pack_A/Assets/Pack_a/Scripts/handle/MoveHandle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MoveHandle : MonoBehaviour,IPointerDownHandler,IPointerUpHandler
+{
+    private bool isHolding = false;
+    private Transform target;
+
+    void Start()
+    {
+        target = transform;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isHolding = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isHolding = false;
+    }
+
+    void Update()
+    {
+        if (!isHolding)
+            return;
+
+        var mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.transform.position.z * -1;
+        target.position = Camera.main.ScreenToWorldPoint(mousePos);
+
+        var scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll > 0) target.rotation *= Quaternion.Euler(0, 0, 30);
+        if (scroll < 0) target.rotation *= Quaternion.Euler(0, 0, -30);
+    }
+}
