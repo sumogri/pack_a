f295de3a6ae941310a3324f469a97ef9d398f3ef
kazushi sakashita <kazusi@hotmail.co.jp>
2019-07-07 02:20:52 +0900

    [add] 1-N 系のステージを追加

--- UnityProject/Pack_A/Assets/Pack_a/Scripts/TwieetButton.cs
+++ UnityProject/Pack_A/Assets/Pack_a/Scripts/TwieetButton.cs
@@ -7,17 +7,18 @@ using naichilab;
 public class TwieetButton : MonoBehaviour
 {
     [Zenject.Inject] private ScoreModel score;
+    [SerializeField] private string stageName;
 
     // Start is called before the first frame update
     void Start()
-    {
-        
+    {        
         gameObject.GetComponent<Button>()
             .onClick.AddListener(() => Twieet());
     }
 
     private void Twieet()
     {
-        UnityRoomTweet.Tweet("pack_a",$"{score.Score}個の'あ'を詰めました", "unityroom", "unity1week");
+        var content = $"ステージ{stageName}で、{score.Score}個の'あ'を詰めました";
+        UnityRoomTweet.Tweet("pack_a",content, "unityroom", "unity1week");
     }
 }
