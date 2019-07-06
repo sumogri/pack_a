2685af6a5c0f57504b491fca5063f4f065b4975c
kazushi sakashita <kazusi@hotmail.co.jp>
2019-07-07 01:05:13 +0900

    [add] シーン遷移を追加

---
+++ UnityProject/Pack_A/Assets/Pack_a/Scripts/StageChangeButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageChangeButton : MonoBehaviour
+{
+    private readonly string stageSceneNamePrefix = "Stage";
+    [SerializeField] private string stageNo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>()
+            .onClick.AddListener(SceneChange);
+    }
+
+    private void SceneChange()
+    {
+        SceneManager.LoadScene(stageSceneNamePrefix + stageNo);
+    }
+}
