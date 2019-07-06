e065c4c8ca6022241a2d9bedc304af504e4fad61
kazushi sakashita <kazusi@hotmail.co.jp>
2019-07-06 22:58:55 +0900

    [add] ツイート機能を追加

--- UnityProject/Pack_A/Assets/Pack_a/Scripts/TwieetButton.cs
+++ UnityProject/Pack_A/Assets/Pack_a/Scripts/TwieetButton.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using naichilab;
 
 public class TwieetButton : MonoBehaviour
 {
+    [Zenject.Inject] private ScoreModel score;
+
     // Start is called before the first frame update
     void Start()
     {
         
+        gameObject.GetComponent<Button>()
+            .onClick.AddListener(() => Twieet());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Twieet()
     {
-        
+        UnityRoomTweet.Tweet("pack_a",$"{score.Score}個の'あ'を詰めました", "unityroom", "unity1week");
     }
 }
