84c5b530a9eb5322862951526cc7f4179b1abe5d
kazushi sakashita <kazusi@hotmail.co.jp>
2019-07-07 17:53:13 +0900

    [add] 2-2っぽい何かを完成
    うーん……

---
+++ UnityProject/Pack_A/Assets/Pack_a/Scripts/Painter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Painter : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "A") return;
+
+        collision.tag = "Black";
+        var tm = collision.GetComponent<TextMeshProUGUI>();
+        tm.text = "●";
+        tm.fontSize = 2.25f;
+    }
+}
